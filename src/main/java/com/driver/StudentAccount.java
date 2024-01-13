e128938f000dae71146ab2162ae71dbd859a1ea6
Vishal Jangir <136950731+jangir02vishal@users.noreply.github.com>
2024-01-13 21:02:36 +0530

    Update StudentAccount.java

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -1,12 +1,10 @@
 package com.driver;
 
-public class StudentAccount extends BankAccount{
+public class StudentAccount extends BankAccount {
+    private String institutionName;
 
-    String  institutionName;
-
-    public StudentAccount(String name, double balance, String  institutionName) {
-        //minimum balance is 0 by default
-
+    public StudentAccount(String name, double balance, String institutionName) {
+        super(name, balance, 0);
+        this.institutionName = institutionName;
     }
-
 }
